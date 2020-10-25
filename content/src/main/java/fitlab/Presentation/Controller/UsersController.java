5f28baac2b12df31d36277a18f6a856f68c749fa
Hlib Yarovyi <yarovhli@fit.cvut.cz>
2020-10-25 13:51:53 +0100

    All users page implemented

--- content/src/main/java/fitlab/Presentation/Controller/UsersController.java
+++ content/src/main/java/fitlab/Presentation/Controller/UsersController.java
@@ -3,12 +3,17 @@ package fitlab.Presentation.Controller;
 import fitlab.BussinessLogic.Logic.UsersLogic;
 import fitlab.Data.Model.User;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.SessionAttribute;
+
+import java.util.List;
 
 @Controller
 @RequestMapping("/users")
@@ -17,13 +22,21 @@ public class UsersController {
     @Autowired
     private UsersLogic usersLogic;
 
+    @RequestMapping(method = RequestMethod.GET)
+    public String getUsers(Model model){
+        List<User> u = usersLogic.getAllUsers();
+
+        model.addAttribute("users", u);
+        return "allUsers";
+    }
+
     @RequestMapping(value = "/{pathId}", method = RequestMethod.GET)
-    public String getUserPage(@CookieValue(value = "username", defaultValue = "ses") String username, @PathVariable String pathId, Model model){
+    public String getUserPage(@PathVariable String pathId, Model model){
         User u = usersLogic.getUserByPath(pathId);
         if(u==null)      // If user is not exist return "USER NOT FOUND" page
             return "errorpage";
 
-        model.addAttribute("error", username);
+        model.addAttribute("error", "");
         model.addAttribute("user", u);
         return "userPage";
     }
