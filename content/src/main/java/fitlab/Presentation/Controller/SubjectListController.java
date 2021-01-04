efcdf43e2c0ed2981ba5e8362f3902033ec4f8a3
Aydin Misirzade <aydinmisirzade@hotmail.com>
2021-01-04 14:21:21 +0100

    Edit button bug fixed

--- content/src/main/java/fitlab/Presentation/Controller/SubjectListController.java
+++ content/src/main/java/fitlab/Presentation/Controller/SubjectListController.java
@@ -1,14 +1,20 @@
 package fitlab.Presentation.Controller;
 
 import fitlab.BussinessLogic.Logic.SubjectLogic;
+import fitlab.BussinessLogic.Logic.UsersLogic;
 import fitlab.Data.Model.Semester;
+import fitlab.Data.Model.User;
+import fitlab.Data.Repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.Optional;
 
 //The class controls list of subjects
 @Controller
@@ -16,21 +22,21 @@ public class SubjectListController {
 
     @Autowired
     SubjectLogic s_repo;
-
-    /**
-     * This method is used to show subject list page
+    @Autowired
+    UsersLogic usersLogic;
+    /* This method is used to show subject list page
      * @param model This is a variable used by the framework
      * @return This returns a subject list page
      */
     @GetMapping("/subjects")
-    public String subjectList(Model model) {
+    public String subjectList(Model model, Authentication auth) {
+        User u = usersLogic.getUser(auth.getName());
+        model.addAttribute("user", u);
         model.addAttribute("subject", s_repo.SearchList());
         return "subjects";
     }
 
-
-    /**
-     *  This method is used to add a subject to the database
+    /*  This method is used to add a subject to the database
      * @param code This is a code of the subject written in text form
      * @param name This is a name of the subject
      * @param semester This is semester
@@ -59,7 +65,7 @@ public class SubjectListController {
     }
 
     @PreAuthorize("hasAuthority('ADMIN')")
-    @PostMapping(value = "/edit", params = {"id","code","name","semester"})
+    @PostMapping(value = "/subjects", params = {"id","code","name","semester"})
     public String editSub(@RequestParam int id, @RequestParam String code, @RequestParam String name, @RequestParam int semester) {
         if(code.isEmpty() || name.isEmpty())
             return "errorpage";
