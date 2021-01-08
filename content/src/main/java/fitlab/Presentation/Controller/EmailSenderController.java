8331981c4ebac5dfea2685afc9c547bccbee2e3c
Hlib Yarovyi <yarovhli@fit.cvut.cz>
2021-01-08 20:35:10 +0200

    Back-end contact with front-end fully implemented

--- content/src/main/java/fitlab/Presentation/Controller/EmailSenderController.java
+++ content/src/main/java/fitlab/Presentation/Controller/EmailSenderController.java
@@ -2,8 +2,11 @@ package fitlab.Presentation.Controller;
 
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 @RequestMapping("/email")
@@ -15,9 +18,18 @@ public class EmailSenderController {
         return "emailSender";
     }
 
+    @RequestMapping(value="/result", method = RequestMethod.GET)
+    public String showEmailSenderResult(@RequestParam(required = true, name = "an") Boolean result, Model model){
+        model.addAttribute("result", result.toString());
+        return "emailSenderResult";
+    }
+
     @RequestMapping(value="/send" ,method = RequestMethod.POST)
-    public String sendEmail(String emailTo, String username, String email, String subject, String message ){
+    public ModelAndView sendEmail(String emailTo, String username, String email, String subject, String message, Model model ){
         System.err.println(emailTo + " " + subject + " " + message);
-        return "emailSender";
+
+        ModelAndView modelAndView = new ModelAndView("redirect:/email/result");
+        modelAndView.addObject("an", "false");
+        return modelAndView;
     }
 }
---
+++ content/src/main/resources/templates/emailSenderResult.ftl
@@ -0,0 +1,10 @@
+<#import "parts/common.ftl" as c>
+
+<@c.page " | Dashboard">
+    </br>
+    </br>
+    </br>
+    <div class="container">
+        <h5>${result}</h5>
+    </div>
+</@c.page>
